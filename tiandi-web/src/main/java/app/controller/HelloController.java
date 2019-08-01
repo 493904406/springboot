018c97b7eb14cb9cb1d7b29de015df0f6fddd87d
yanxubin <yanxubin@imdada.cn>
2019-08-01 10:56:11 +0800

    修改为聚合工程

--- tiandi-web/src/main/java/app/controller/HelloController.java
+++ tiandi-web/src/main/java/app/controller/HelloController.java
@@ -1,4 +1,4 @@
-package com.app.controller;
+package app.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
--- tiandi-web/src/main/java/app/main/App.java
+++ tiandi-web/src/main/java/app/main/App.java
@@ -1,11 +1,11 @@
-package com.app.main;
+package app.main;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
-@SpringBootApplication(scanBasePackages = "com.app")
+@SpringBootApplication(scanBasePackages = "app")
 public class App extends SpringBootServletInitializer {
     public static void main(String [] args){
         SpringApplication.run(App.class,args);
