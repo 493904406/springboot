a3702f25ff26d8c9a522d02949e9088e9ce675ba
yanxubin <yanxubin@imdada.cn>
2019-08-01 20:03:42 +0800

    创建源文件夹

--- tiandi-service/src/main/java/com/app/service/TianDiService.java
+++ tiandi-service/src/main/java/com/app/service/TianDiService.java
@@ -1,4 +1,8 @@
 package com.app.service;
 
 public interface TianDiService {
+
+
+
+
 }
--- tiandi-service/src/main/java/com/app/service/impl/TianDIServiceImpl.java
+++ tiandi-service/src/main/java/com/app/service/impl/TianDIServiceImpl.java
@@ -1,11 +1,10 @@
 package com.app.service.impl;
 
 import com.app.service.TianDiService;
+import org.springframework.stereotype.Service;
 
+@Service
 public class TianDIServiceImpl implements TianDiService {
 
 
-    private void dddddd(){
-
-    }
 }
